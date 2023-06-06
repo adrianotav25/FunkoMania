2f75b2a2a0f0143cdac0a32993c4650492d716e7
Adriano <adrianotpereira3@gmail.com>
2023-06-06 14:52:07 -0300

    Client Application

---
+++ FunkoMania.Application/Interfaces/IClientAppService.cs
@@ -0,0 +1,30 @@
+﻿using FunkoMania.Application.ViewModel;
+using FunkoMania.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkoMania.Application.Interfaces
+{
+    public interface IClientAppService
+    {
+        Task<ClientViewModel> AddAsync(ClientViewModel viewModel);
+        ClientViewModel Update(ClientViewModel viewModel);
+        
+        void remove(Guid id);
+        void remove(Expression<Func<Client, bool>> expression);
+
+        ClientViewModel GetById(Guid id);
+        Task<ClientViewModel> GetByIdAsync(Guid id);
+
+        IEnumerable<ClientViewModel> Search(Expression<Func<Client, bool>> predicate);
+        Task<IEnumerable<ClientViewModel>> SearchAsync(Expression<Func<Client, bool>> predicate);
+        IEnumerable<ClientViewModel> Search(Expression<Func<Client, bool>> predicate,
+            int pageNumber,
+            int pageSize);
+
+    }
+}
---
+++ FunkoMania.Application/Services/ClientAppService.cs
@@ -0,0 +1,100 @@
+﻿using AutoMapper;
+using FunkoMania.Application.Interfaces;
+using FunkoMania.Application.ViewModel;
+using FunkoMania.Domain.Entities;
+using FunkoMania.Domain.Interfaces;
+using FunkoMania.Domain.Shared.Transaction;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkoMania.Application.Services
+{
+    public class ClientAppService : BaseService, IClientAppService
+    {
+        protected readonly IClientRepository _repository;
+        protected readonly IMapper _mapper;
+
+        public ClientAppService(IClientRepository repository,
+            IMapper mapper,
+            IUnitOfWork unitOfWork,
+            IMediator bus) : base(unitOfWork, bus)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public ClientViewModel GetById(Guid id)
+        {
+            var domain = _repository.GetById(id);
+            var viewModel = _mapper.Map<ClientViewModel>(domain);
+            return viewModel;
+        }
+
+        public async Task<ClientViewModel> GetByIdAsync(Guid id)
+        {
+            var domain = await _repository.GetByIdAsync(id);
+            var viewModel = _mapper.Map<ClientViewModel>(domain);
+            return viewModel;
+        }
+
+        public void remove(Guid id)
+        {
+            _repository.Remove(id);
+            Commit();
+        }
+
+        public void remove(Expression<Func<Client, bool>> expression)
+        {
+            _repository.Remove(expression);
+            Commit();
+        }
+
+        public IEnumerable<ClientViewModel> Search(Expression<Func<Client, bool>> predicate)
+        {
+            var domain = _repository.Search(predicate);
+            var viewModels = _mapper.Map<IEnumerable<ClientViewModel>>(domain);
+            return viewModels;
+        }
+
+        public IEnumerable<ClientViewModel> Search(Expression<Func<Client,
+            bool>> predicate,
+            int pageNumber,
+            int pageSize)
+        {
+            var domain = _repository.Search(predicate, pageNumber, pageSize);
+            var viewModels = _mapper.Map<IEnumerable<ClientViewModel>>(domain);
+            return viewModels;
+        }
+
+        public async Task<IEnumerable<ClientViewModel>> SearchAsync(Expression<Func<Client, bool>> predicate)
+        {
+            var domain = await _repository.SearchAsync(predicate);
+            var viewModels = _mapper.Map<IEnumerable<ClientViewModel>>(domain);
+            return viewModels;
+        }
+
+        public async Task<ClientViewModel> AddAsync(ClientViewModel viewModel)
+        {
+            Client domain = _mapper.Map<Client>(viewModel);
+            domain = await _repository.AddAsync(domain);
+            Commit();
+
+            ClientViewModel viewmodelReturn = _mapper.Map<ClientViewModel>(domain);
+            return viewmodelReturn;
+        }
+
+        public ClientViewModel Update(ClientViewModel viewModel)
+        {
+            var domain = _mapper.Map<Client>(viewModel);
+            domain = _repository.Update(domain);
+            Commit();
+            ClientViewModel viewModelReturn = _mapper.Map<ClientViewModel>(domain);
+            return viewModelReturn;
+        }
+    }
+}
